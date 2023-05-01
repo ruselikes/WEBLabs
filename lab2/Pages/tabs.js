65dcdf7609c584f82482b8604fd269c707b163d8
ruselikes <vipsteam2002@mail.ru>
2023-05-01 20:37:47 +0300

    Точно готовая лаба 6 с TOBUY листом и формой для создания записи

---
+++ lab2/Pages/tabs.js
@@ -0,0 +1,48 @@
+
+    const tabsContainer = document.querySelector('.container--tabs');
+    const contentContainer = document.querySelector('.container--content');
+
+
+    fetch('../data.json')
+    .then(response => response.json())
+    .then(data => {
+    //  Создание вкладок (li В ul)
+    data.forEach((iphone, index) => {
+        const tab = document.createElement('li');
+        tab.className = 'tab' + (index == 0 ? ' tab--active' : '');
+        tab.textContent = iphone.title;
+        tabsContainer.appendChild(tab);
+
+        // Создание контетной части
+        const content = document.createElement('div');
+        content.className = 'content' + (index == 0 ? ' content--active' : '');
+        content.innerHTML = `
+                        <h2>${iphone.title}</h2>
+                        <p>${iphone.description}</p>
+                        <iframe
+                            width="100%"
+                            height="315"
+                            src="${iphone.videoUrl}"
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen>
+                        </iframe>`;
+        contentContainer.appendChild(content);
+    });
+
+    // Для каждой вкладки добавлю лисенеры по клику
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+    // Сделаю все неактивным( и вкладка, и контент)
+    tabs.forEach(tab => tab.classList.remove('tab--active'));
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(content => content.classList.remove('content--active'));
+
+    // Сделаю вкладку по которой кликнули активной, и соответсвующий (по индексу) ее контент тоже
+    tab.classList.add('tab--active');
+    const content = contents[index];
+    content.classList.add('content--active');
+});
+});
+});
---
+++ lab2/Pages/todoscript.js
@@ -0,0 +1,46 @@
+
+    const todoForm = document.getElementById("todo-form");
+    const todoTitle = document.getElementById("todo-title");
+    const todoDescription = document.getElementById("todo-description");
+    const todoList = document.getElementById("todo-list");
+    // создаем пустой массив для хранения задач
+    let todos = [];
+
+    // обработчик отправки формы
+    todoForm.addEventListener("submit", (event) => {
+    // отменяем стандартное поведение браузера при отправке формы
+    event.preventDefault();
+
+    // получаем значение заголовка и описания задачи из полей формы
+    const title = todoTitle.value;
+    const description = todoDescription.value;
+
+    // создаем объект задачи и добавляем его в массив todos
+    const todo = { title, description };
+    todos.push(todo);
+
+    // создаем элемент списка задач и добавляем его в todoList
+    // const todoItem = document.createElement("li");
+    // todoItem.innerText = `${todo.title}: ${todo.description}`;
+    // todoList.appendChild(todoItem);
+
+    // очищаем поля формы
+    todoTitle.value = "";
+    todoDescription.value = "";
+    showTodoList();
+    });
+    // функция для отображения списка задач
+    function showTodoList() {
+    // очищаем текущий список задач
+    todoList.innerHTML = "";
+
+    // создаем элементы списка задач и добавляем их в todoList
+    for (let i = 0; i < todos.length; i++) {
+    const todoItem = document.createElement("li");
+    todoItem.innerText = `${i+1 +')  ' + todos[i].title}: ${todos[i].description}`;
+    todoList.appendChild(todoItem);
+    }
+    }
+
+    // отображаем список задач при загрузке страницы
+
